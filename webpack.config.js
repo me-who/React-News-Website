0f844ea71a497b75ad1306bc2a63572aa4b905e6
lee <Hao_Lee7214@163.com>
2017-09-27 12:51:57 +0800

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: [
+    "./src/js/root.js"
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query:
+        {
+          presets: ['react', 'es2015']
+        }
+      },
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      {
+  			test:/\.less$/,
+  			exclude:/node_modules/,
+  			use:[{
+  				loader:'style-loader'
+  			},{
+  				loader:'css-loader'
+  			},{
+  				loader:'less-loader'
+  			}]
+		  }
+    ]
+  },
+  output: {
+    path: __dirname,
+    filename: "./src/bundle.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
